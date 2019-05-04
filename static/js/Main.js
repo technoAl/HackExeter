71873e93232e349681c7f63c78a8eb349638c263
Jerry Han <jhan6501@gmail.com>
2019-05-04 13:42:43 -0400

    broken button help me pls

--- static/js/Main.js
+++ static/js/Main.js
@@ -1,8 +1,9 @@
 document.getElementById("button").addEventListener("click", addCommand);
-
 var commands = [];
 
+console.log(addCommand());
 function addCommand (){
+    //console.log("hiS");
     fetch('http://localhost:5000/', {method: "POST"})
         .then(function(response) {
             return response.text();
@@ -23,14 +24,23 @@ function createCommandElement (command){
     if (command.equals('up')){
         const elem = document.createElement('img');
         elem.src = "../images/upArrow.png";
-        const parentElement = // find the parent element
+        const parentElement = document.getElementById('parent')// find the parent element
         parentElement.appendChild(elem);
     } else if (command.equals('down')){
-
+        const elem = document.createElement('img');
+        elem.src = "../images/downArrow.png";
+        const parentElement = document.getElementById('parent')// find the parent element
+        parentElement.appendChild(elem);
     } else if (command.equals('left')){
-
+        const elem = document.createElement('img');
+        elem.src = "../images/leftArrow.png";
+        const parentElement = document.getElementById('parent')// find the parent element
+        parentElement.appendChild(elem);
     } else if(command.equals('right')){
-
+        const elem = document.createElement('img');
+        elem.src = "../images/rightArrow.png";
+        const parentElement = document.getElementById('parent')// find the parent element
+        parentElement.appendChild(elem);
     }
 }
     
